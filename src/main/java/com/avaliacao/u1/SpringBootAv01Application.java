5cc7c36db3498b775276fe769e0c1fff321f33ab
Nicolas Braz <nicolasfernandobraz@gmail.com>
2018-03-22 09:32:36 -0300

    Primeira Parte da Avaliação

---
+++ src/main/java/com/avaliacao/u1/SpringBootAv01Application.java
@@ -0,0 +1,12 @@
+package com.avaliacao.u1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAv01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAv01Application.class, args);
+	}
+}
